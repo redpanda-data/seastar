6846454120f190a160d7584b51879da01add3e0b
Rob Blafford <rob@redpanda.com>
2024-02-09 14:56:35 -0500

    net: openssl impl of tls::dh_params

--- src/net/ossl.cc
+++ src/net/ossl.cc
@@ -20,5 +20,85 @@
  */
 
 #include <seastar/net/tls.hh>
+#include <openssl/evp.h>
+#include <openssl/pem.h>
+#include <openssl/bio.h>
 
-namespace seastar {}
+namespace seastar {
+
+template<typename T, auto fn>
+struct ssl_deleter {
+    void operator()(T* ptr) { fn(ptr); }
+};
+
+template<typename T, auto fn>
+using ssl_handle = std::unique_ptr<T, ssl_deleter<T, fn>>;
+
+using bio_ptr = ssl_handle<BIO, BIO_free>;
+using evp_pkey_ptr = ssl_handle<EVP_PKEY, EVP_PKEY_free>;
+
+class tls::dh_params::impl {
+public:
+    static int level_to_bits(level l) {
+        switch (l) {
+            case level::LEGACY:
+                return 1776;
+            case level::MEDIUM:
+                return 2432;
+            case level::HIGH:
+                return 3248;
+            case level::ULTRA:
+                return 15424;
+            default:
+                throw std::runtime_error(format("Unknown value of dh_params::level: {:d}", static_cast<std::underlying_type_t<level>>(l)));
+        }
+    }
+
+    static evp_pkey_ptr make_evp_pkey(level l) {
+        /// Instantiate new Diffie-Hellman key context
+        EVP_PKEY *pkey = nullptr;
+        EVP_PKEY_CTX *pctx = EVP_PKEY_CTX_new_from_name(nullptr, "DH", nullptr);
+
+        OSSL_PARAM params[3];
+        unsigned int bits = level_to_bits(l);
+        char group[] = "group";
+        params[0] = OSSL_PARAM_construct_utf8_string("type", group, strlen(group));
+        params[1] = OSSL_PARAM_construct_uint("pbits", &bits);
+        params[2] = OSSL_PARAM_construct_end();
+
+        EVP_PKEY_keygen_init(pctx);
+        EVP_PKEY_CTX_set_params(pctx, params);
+        EVP_PKEY_generate(pctx, &pkey);
+        EVP_PKEY_CTX_free(pctx);
+        return evp_pkey_ptr(pkey);
+    }
+
+    impl(level l) : _pkey(make_evp_pkey(l)) {}
+
+    impl(const blob& pkcs3, x509_crt_format fmt)
+        : _pkey([&] {
+            bio_ptr key_bio(BIO_new_mem_buf(pkcs3.begin(), pkcs3.size()));
+            auto pkey_temp = EVP_PKEY_new();
+            if(fmt == x509_crt_format::PEM) {
+                if (nullptr == PEM_read_bio_Parameters(key_bio.get(), &pkey_temp)) {
+                    EVP_PKEY_free(pkey_temp);
+                    throw std::system_error(0, error_category());
+                }
+            } else if (fmt == x509_crt_format::DER) {
+                if(nullptr == d2i_KeyParams_bio(EVP_PKEY_DH, &pkey_temp, key_bio.get())){
+                    EVP_PKEY_free(pkey_temp);
+                    throw std::system_error(0, error_category());
+                }
+            } else {
+                throw std::invalid_argument("Unknown x509_crt_format selected");
+            }
+            return evp_pkey_ptr(pkey_temp);
+        }())
+    {}
+
+    operator EVP_PKEY*() const { return _pkey.get(); }
+
+private:
+    evp_pkey_ptr _pkey;
+};
+}
