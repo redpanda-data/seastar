8e6e45269d7e9786146ccff4fd7c182b1af10fc9
Rob Blafford <rob@redpanda.com>
2024-02-27 09:59:53 -0500

    net: Remove access to cert_creds _impl
    - Now that code has been moved around the private _impl within the
    certificate credentials class cannot be accessed externally due to the
    fact that impl is forward defined in tls.hh
    
    - Solution is to create new method on the certificate_credentials class
    that will forward the call to the underlying impl

--- include/seastar/net/tls.hh
+++ include/seastar/net/tls.hh
@@ -142,6 +142,10 @@ namespace tls {
      */
     using dn_callback = noncopyable_function<void(session_type type, sstring subject, sstring issuer)>;
 
+    enum class client_auth {
+        NONE, REQUEST, REQUIRE
+    };
+
     /**
      * Holds certificates and keys.
      *
@@ -224,6 +228,9 @@ namespace tls {
          */
         std::optional<std::vector<cert_info>> get_trust_list_info() const noexcept;
 
+        /// TODO(rob) comment these
+        void enable_load_system_trust();
+        void set_client_auth(client_auth);
     private:
         class impl;
         friend class session;
@@ -239,10 +246,6 @@ namespace tls {
     class verification_error : public std::runtime_error {
     public:
         using runtime_error::runtime_error;
-    };
-
-    enum class client_auth {
-        NONE, REQUEST, REQUIRE
     };
 
     /**
