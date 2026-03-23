ba8ab670ed814dfb8d76337052003e9ebfa48f92
Stephan Dollberg <stephan@redpanda.com>
2026-03-23 09:50:52 +0000

    help with promise::clear and promise_base::make_ready inlineability

--- include/seastar/core/future.hh
+++ include/seastar/core/future.hh
@@ -790,7 +790,10 @@ protected:
     void move_it(promise_base&& x) noexcept;
     promise_base(promise_base&& x) noexcept;
 
-    void clear() noexcept;
+    inline void clear() noexcept;
+
+    // clear_on_broken() handles the clear() slow path when _task is non-null.
+    void clear_on_broken() noexcept;
 
     // We never need to destruct this polymorphicly, so we can make it
     // protected instead of virtual
@@ -802,7 +805,16 @@ protected:
     promise_base& operator=(promise_base&& x) noexcept;
 
     template<urgent Urgent>
-    void make_ready() noexcept;
+    void make_ready() noexcept {
+        if (_task) {
+            assert_task_shard();
+            if constexpr (Urgent == urgent::yes) {
+                ::seastar::schedule_urgent(std::exchange(_task, nullptr));
+            } else {
+                ::seastar::schedule(std::exchange(_task, nullptr));
+            }
+        }
+    }
 
     template<typename T>
     void set_exception_impl(T&& val) noexcept {
@@ -1120,6 +1132,16 @@ protected:
 
     friend class promise_base;
 };
+
+inline void promise_base::clear() noexcept {
+    if (__builtin_expect(bool(_task), false)) {
+        clear_on_broken();
+        return;
+    }
+    if (_future) {
+        _future->detach_promise();
+    }
+}
 
 template <typename Func, typename... T>
 struct future_result  {
